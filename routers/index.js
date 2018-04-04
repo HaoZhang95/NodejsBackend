387cebb297d62851f79793d5b67f547527942eb0
hao zhang <haozh@metropolia.fi>
2018-04-04 20:48:12 +0300

    make it better

--- routers/index.js
+++ routers/index.js
@@ -6,7 +6,7 @@ const router = express.Router()   /* 使用创建模块化,可挂载的句柄 */
 
 router.get('/', (req, res) =>{
     res.send("<h2>Welcome to backend of Team 3. Please use proper api to get needed json. :) </h2>" +
-        "<h3><br><br>Project Members (No Order): <br><br> Hao Zhang <br><br> Mikael Rämö <br><br> Emma Honkala <br><br> Long Nguyen</h3>")
+        "<h4><br>Project Members (No Order):  Hao Zhang , Mikael Rämö , Emma Honkala , Long Nguyen</h4>")
 })
 
 module.exports = router
